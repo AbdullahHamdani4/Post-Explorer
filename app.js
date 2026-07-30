44a914f726e8b9be83b42ae73de8f1f57386f3bd
Abdullah Hamdani <abdullah.hamdani87@gmail.com>
2026-07-30 13:15:32 +0500

    Enhance post item interactivity with click event for details and improve CSS styling

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const searchBar=document.getElementById("searchInput")
 const postList=document.getElementById("postList");
 const postDetail=document.getElementById("postDetail");
-
 const apiDataAll=async()=>{
     const response=await fetch("https://dummyjson.com/posts");
     const data=await response.json();
     const posts=data.posts;
     const mappedPost=posts.map((each)=>{
         return `
-        <div class="post-item active" id="post-1">
+        <div class="post-item active" id="post-1" onclick=showDetail(${each.id})>
         <span class="post-index">${each.id}</span>
         <h3 class="post-title">${each.title}</h3>
         <p class="post-body truncate-2">${each.body}</p>
@@ -20,8 +19,7 @@ const apiDataAll=async()=>{
     });
     postList.innerHTML=mappedPost.join("");
   };
-// console.log();
-apiDataAll()
+ apiDataAll()
 
 
 const apiDataSearch=async(query)=>{
@@ -30,8 +28,7 @@ const apiDataSearch=async(query)=>{
     const posts=data.posts;
     const mappedPost=posts.map((each)=>{
         return `
-        <div class="post-item active" id="post-1" onclick="showDetail(${each})"
-        >
+        <div class="post-item active" id="post-1" onclick="showDetail(${each.id})">
         <span class="post-index">${each.id}</span>
         <h3 class="post-title">${each.title}</h3>
         <p class="post-body truncate-2">${each.body}</p>
@@ -45,18 +42,21 @@ const apiDataSearch=async(query)=>{
      
 };
 
-const showDetail=(post)=>{
- showDetail.innerHTML=`
-  <span class="detail-index">${each.id}</span>
-      <h2 class="detail-title">${each.title}</h2>
+const showDetail=async (id)=>{
+   const response=await fetch(`https://dummyjson.com/posts/${id}`);
+    const data=await response.json();
+    postDetail.innerHTML=`
+     <span class="detail-index">${data.id}</span>
+      <h2 class="detail-title">${data.title}</h2>
       <div class="detail-meta">
-        <span class="detail-reactions">${each.reactions.likes}</span>
-        <span class="detail-views">${each.views} views</span>
-       </div>
-      <p class="detail-body">${each.body}</p>
+        <span class="detail-reactions">${data.reactions.likes} likes</span>
+        <span class="detail-views">${data.views} views</span>
+        <span class="detail-tags">history, crime</span>
+      </div>
+      <p class="detail-body">${data.body}</p>
 
- `
-}
+    `
+ }
 searchBar.addEventListener("input",()=>{
     if(searchBar.value.toLowerCase().trim()){
         apiDataSearch(searchBar.value.toLowerCase().trim());
